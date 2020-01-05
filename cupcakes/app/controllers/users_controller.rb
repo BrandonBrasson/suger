28ec18e6ad38ae6820a0a290246b4e352458e492
Brandon Brasson <rocksong622@gmail.com>
2020-01-05 19:56:15 +0000

    Add existing file

--- cupcakes/app/controllers/users_controller.rb
+++ cupcakes/app/controllers/users_controller.rb
@@ -2,5 +2,23 @@ require './config/environment'
 
 class UserController < ApplicationController
 
+get '/login' do
+erb :login
+end
+
+post "/login" do
+user = User.find_by(email: params[:email])
+if user && user.authenticate(params[:password])
+session[:user_id] = user.id
+redirect "/users/#{user.id}"
+else
+  redirect '/login'
+end
+
+get "/user/:id" do
+  redirect '/redirect'
+end
+end
+
 
 end
--- cupcakes/app/models/user.rb
+++ cupcakes/app/models/user.rb
@@ -1,26 +1,9 @@
 class User < ActiveRecord::Base
   has_many :cupcakes
-#  has_many :
+
 
 
   has_secure_password
-  validates_presence_of :username
-  validates :username, uniqueness: true
-
-  def delete_cupcakes
-    self.cupcakes.collect {|b| b.cupcake}
-  end
-
-  def add_delete(cupcake)
-  Cupcake.create(user_id: self.id, cupcake_id: cupcake.id)
-    save
-  end
-
-  def remove_delete(cupcake)
-    cupcake = Cupcake.find_by(user_id: self.id,  cupcake_id: cupcake.id)
-    cupcake.destroy
-    save
-  end
-
+  
 
 end
