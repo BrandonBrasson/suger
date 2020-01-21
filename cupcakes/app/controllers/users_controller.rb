96a38f24554a8faead5a8cd7d33e021b5695fd16
Brandon Brasson <31114139+BrandonBrasson@users.noreply.github.com>
2020-01-21 18:09:35 -0500

    Update users_controller.rb

--- cupcakes/app/controllers/users_controller.rb
+++ cupcakes/app/controllers/users_controller.rb
@@ -25,22 +25,15 @@ get "/users/:id" do
   erb :show
 end
 
-
-  get '/signup' do
-   user = User.find_by(username: params[:username])
-if user && user.authenticate(params[:password])
-      @user = username
-      redirect "/login"
-    else
-      erb :signup
-    end
-  end
-
 post '/signup' do
-  @user = User.create(params[:user])
+if  @user = User.create(params[:user])
     session[:name] = @user.username
     session[:user_id] = @user.id
-  end
+  redirect "/login"
+else
+  redirect "/signup"
+end
+end
 #Redirect a user to their show page once they've successfully logged in
 #I#mplement sign up functionality by creating a new user and adding their `user_id` to the sessions hash
 #Implement logout by creating a route that clears the session hash
